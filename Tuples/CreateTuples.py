8fda9d7f18dffab2d34992ab852de250167df1d6
Sanjana K R <sanjanakr97@gmail.com>
2020-08-12 16:22:43 +0530

    Refactor: modified by changing the input only to be integers

--- Tuples/CreateTuples.py
+++ Tuples/CreateTuples.py
@@ -5,7 +5,7 @@ size=(int(input("Enter the size of the tuples ")))
 
 #loop to add items in list
 for i in range(size):
-    demoList.append(input("Enter the value in list "))
+    demoList.append(int(input("Enter the integers value in list ")))
 
 tupleEx=tuple(demoList) #coverts list to tuple and then assigns to tupleEx
 print(tupleEx)
