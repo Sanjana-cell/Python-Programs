6925ab67c84b879cc079dd1578f3275735ac2cd4
Sanjana K R <sanjanakr97@gmail.com>
2020-08-12 20:15:57 +0530

    Refactor: add comments

--- Strings/AddString.py
+++ Strings/AddString.py
@@ -3,8 +3,14 @@ least 3). If the given string already ends with 'ing' then add 'ly' instead. If 
 given string is less than 3, leave it unchanged"""
 
 def addSubString(string):
+    """
+    checks if string length more the or equal to 3 then adds ing at the end of string if ing already present the adds ly at the end
+     of the string and returns the string. if string is less than 3 string returned unchanged
+    :param string: string
+    :return: retrun the updated string with ing or ly
+    """
     if len(string) >= 3:
-        if string[len(string)-3:len(string)] != "ing":
+        if string[len(string)-3:len(string)] != "ing": #checks if end of the string contains ing
             string += "ing"
         else:
             string += "ly"
