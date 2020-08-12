b1be07115a594f0b7ed10630338e801500e921f8
Sanjana K R <sanjanakr97@gmail.com>
2020-08-12 19:58:04 +0530

    Refactor: changed the logic of the program

--- Strings/ReplaceCharacter.py
+++ Strings/ReplaceCharacter.py
@@ -3,19 +3,19 @@ Program to get a string from a given string where all occurrences of its
 first char have been changed to '$'
 """
 userInput=(input("Enter the String "))
-print("Length of a string",len(userInput))
-length=len(userInput)
-stringList=[]
-newList=[]
 def replaceCharacter(userInput):
-    str=""
-    for x in userInput:
-        if x not in stringList and x != "$":
-            stringList.append(x)
-        else:
-            userInput=userInput.replace(x,"$")
-            print("from else",userInput)
-    print(str)
-    return userInput
+    """
+    it replaces all occurrences of string first char have been changed to '$
+    :param userInput: string
+    :return: returns changed string
+    """
+    firstChar=userInput[0] #assigns first character in string
+    newString=userInput[1:len(userInput)] # assigns from character 2 to last character of the string to new string
+    for i in range(len(newString)):
+        if firstChar == newString[i]:
+            newString=newString.replace(newString[i],"$")
+
+    newString=firstChar+newString #concatenates firstChar and new String
+    return newString
 print(replaceCharacter(userInput))
 
