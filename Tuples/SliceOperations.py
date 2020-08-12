ae5ccd9c3b1c122bcf583c599e93630a2ad5c66b
Sanjana K R <sanjanakr97@gmail.com>
2020-08-12 17:43:54 +0530

    Added: slice a tuples

---
+++ Tuples/SliceOperations.py
@@ -0,0 +1,15 @@
+#  program to slice a tuple
+
+my_tuple = ('s','a','n','j','a','n','a','k','r')
+
+# elements 2nd to 4th
+print(my_tuple[1:4])
+
+# elements beginning to 2nd
+print(my_tuple[:2])
+
+# elements 8th to end
+print(my_tuple[7:])
+
+# elements beginning to end
+print(my_tuple[:])
