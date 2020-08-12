57c91b3099af15f23b5250c2452248c3d487f89e
Sanjana K R <sanjanakr97@gmail.com>
2020-08-12 10:22:10 +0530

    Added: adds the member in set

---
+++ Sets/AddElements.py
@@ -0,0 +1,19 @@
+setEx=set()
+
+def addElement(mySet,value):
+    """
+    add the element in the set
+    :param mySet: set
+    :param value: value to be added in the set
+    :return: return the updated set
+    """
+    mySet.add(value)
+    return mySet
+
+size=(int(input("Enter the size of the size ")))
+for i in range(size):
+    setEx.add(int(input("Enter the unique elements in set ")))
+
+print("Before adding the element",setEx)
+element=(int(input("Enter the element to add in the set ")))
+print("After adding the element",addElement(setEx,element))
