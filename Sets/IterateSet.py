feda104c010fd11a9d06ddc80a54bbb41c0fde4c
Sanjana K R <sanjanakr97@gmail.com>
2020-08-12 10:06:02 +0530

    Added: iterates overs the set using loops

---
+++ Sets/IterateSet.py
@@ -0,0 +1,7 @@
+setEx=set()
+size=(int(input("Enter the size of the size ")))
+for i in range(size):
+    setEx.add(int(input("Enter the unique elements in set ")))
+print("Printing the set elements")
+for x in setEx:
+    print(x,end=' ')
