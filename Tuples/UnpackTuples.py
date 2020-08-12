bfb42d3b8b8ec3074c5a360f876ee0735a5346d4
Sanjana K R <sanjanakr97@gmail.com>
2020-08-12 16:37:30 +0530

    Added: unpacks the tuple elements in different varibales

---
+++ Tuples/UnpackTuples.py
@@ -0,0 +1,12 @@
+# Program to unpack a tuple in several variables
+tupleEx=(1,2,"arav","sanjana","vinay")
+
+#unpacks values  of tuples in variables
+(num1,num2,name1,name2,name3)=tupleEx
+
+print(tupleEx)
+print("variable 1=",num1)
+print("variable 2=",num2)
+print("variable 3=",name1)
+print("variable 4=",name2)
+print("variable 5=",name3)
