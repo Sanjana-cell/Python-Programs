8fb015c2e5d9b582562cd6d148dcc2ce521c9b36
Sanjana K R <sanjanakr97@gmail.com>
2020-08-12 11:07:23 +0530

    Added: creates a the symmetric difference between two sets

---
+++ Sets/SymmetricDifference.py
@@ -0,0 +1,18 @@
+set_1=set()
+set_2=set()
+def createSet():
+    """
+    creates the set
+    :return: returns the set
+    """
+    mySet=set()
+    size=(int(input("Enter the size of the size ")))
+    for i in range(size):
+        mySet.add(int(input("Enter the unique elements in set ")))
+    return mySet
+
+set_1=createSet()
+set_2=createSet()
+print("Set 1",set_1)
+print("Set 2",set_2)
+print("Symmetric Difference of the Set_1 - Set_2",set_1.symmetric_difference(set_2))
