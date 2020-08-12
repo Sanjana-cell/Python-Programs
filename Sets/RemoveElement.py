768e9e7e0bff31c3258e284e6b7e6b6cd2d145a8
Sanjana K R <sanjanakr97@gmail.com>
2020-08-12 10:29:05 +0530

    Added: removes the item in the set

---
+++ Sets/RemoveElement.py
@@ -0,0 +1,22 @@
+setEx=set()
+
+def removeElement(mySet,value):
+    """
+    remove the element in the set
+    :param mySet: set
+    :param value: value to be added in the set
+    :return: return the updated set
+    """
+    if value in mySet:
+        mySet.remove(value)
+    else:
+        print("No such element")
+    return mySet
+
+size=(int(input("Enter the size of the size ")))
+for i in range(size):
+    setEx.add(int(input("Enter the unique elements in set ")))
+
+print("Before removing the element",setEx)
+element=(int(input("Enter the element to remove from the set ")))
+print("After removing the element",removeElement(setEx,element))
