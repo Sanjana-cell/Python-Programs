77d54a80cbdda2bf1a1447d2967b2f3ff75d97b9
Sanjana K R <sanjanakr97@gmail.com>
2020-08-12 10:49:52 +0530

    Added: create the union of the sets

---
+++ Sets/Union.py
@@ -0,0 +1,18 @@
+set_1=set()
+set_2=set()
+def createSet():
+    """
+    creates the set
+    :return: returns the set
+    """
+    mySet=set()
+    size=(int(input("Enter the size of the size ")))
+    for i in range(size):
+        mySet.add(int(input("Enter the unique elements in set ")))
+    return mySet
+
+set_1=createSet()
+set_2=createSet()
+print(set_1)
+print(set_2)
+print("Union of the Sets",set_1.union(set_2))
