be728ea64ef5ebdc38d3e74d70d7b25f157f02c9
Sanjana K R <sanjanakr97@gmail.com>
2020-08-15 00:37:22 +0530

    Refactor: added program description and comments

--- Logical/StopWatch.py
+++ Logical/StopWatch.py
@@ -1,3 +1,7 @@
+"""
+Program for measuring the time that elapses between
+the start and end clicks
+"""
 from time import time
 class StopWatch:
     startTimer=0
@@ -5,14 +9,24 @@ class StopWatch:
     elapsed=0
 
     def start(self):
+        """
+        function to calculating start time in milliseconds
+        """
         self.startTimer=int(time() * 1000)
         print("Start Time ", self.startTimer)
 
     def stop(self):
+        """
+        function for calculating stop time in milliseconds
+        """
         self.stopTimer=int(time() * 1000)
         print("Stop Time ", self.stopTimer)
 
     def getElapsed(self):
+        """
+        function to get elapsed time
+        :return: returns the elapsed time
+        """
         self.elapsed= self.stopTimer - self.startTimer
         return self.elapsed
 
@@ -24,20 +38,20 @@ watch=StopWatch()
 value=1
 while value==1:
     try:
-        userInput=(int(input("Press  1 to start Time")))
+        userInput=(int(input("Press  1 to start Time"))) # press 1 to start time
         if userInput != 1:
             raise ValueNotEqualError
-        watch.start()
+        watch.start() #function call to start times
         print()
 
-        userInput = (int(input("Press  2 to stop Time")))
+        userInput = (int(input("Press  2 to stop Time"))) #press 2 to stop time
         if userInput != 2:
             raise ValueNotEqualError
-        watch.stop()
+        watch.stop() #function call to stop timer
         print()
 
-        elapsed=watch.getElapsed()
-        print("Total time Elapsed(in secondes) is ",int(elapsed/1000))
+        elapsed=watch.getElapsed() #function call to get elapsed time after stoping the timer
+        print("Total time Elapsed(in secondes) is ",int(elapsed/1000)) #prints elasped time in seconds
         break
     except ValueNotEqualError:
         print("Enter 1 to start timer and 2 to stop timer")
