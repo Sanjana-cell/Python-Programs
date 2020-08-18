a7697aa1a34ab1df2f6e4f40826de0d99adde819
Sanjana K R <sanjanakr97@gmail.com>
2020-08-18 13:40:35 +0530

    Added: calulates total price of each items from data.json file

---
+++ OOPS/Inventory.py
@@ -0,0 +1,57 @@
+import json
+
+class Inventory :
+    filePath=""
+    def __init__(self,file):
+        self.filePath=file
+
+    def openFile(self):
+        """
+        opens the json file and converts the json object into dictionary
+        :return: returns the dictionary
+        """
+        try:
+            f = open(self.filePath)
+            data = dict(json.load(f))
+            print("Opened file successfully")
+        except:
+            traceback.print_exception(*sys.exc_info())
+        return data
+
+    def calculation(self,data):
+        """
+        calculates the total price of each items
+        :param data: dictionary
+        :return: returns updated dictionary with total price key value
+        """
+        for sub in data.values():
+            for key, ele in sub.items():
+                result=ele['price_per_kg']*ele['weight']
+                print("Total price of ",key , result)
+                ele['totalPrice']=result
+        newData=json.dumps(data)
+        print(newData)
+        print("Calulated the total price of each items")
+        return newData
+
+    def createNewFile(self,newDetails):
+        """
+        creats new json file from the dictionary
+        :param newDetails: dictionary
+        """
+        try:
+            with open("UpdatedInventoryDetail.json", "w") as outfile:
+                outfile.write(newDetails)
+                print("Created new file")
+        except:
+            traceback.print_exception(*sys.exc_info())
+
+path='data.json'
+stock = Inventory(path) #object instantiated
+try:
+    data=stock.openFile() #function call to open json file
+    newData=stock.calculation(data=data) #function call to calculate total price of each items
+except:
+    traceback.print_exception(*sys.exc_info())
+
+stock.createNewFile(newDetails=newData) #functiona call to create new json file
